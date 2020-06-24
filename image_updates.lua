128245c5b7906dc4d2894c9d3670a3e21d006c46
iamafkbot <iamafkbot@gmail.com>
2020-06-24 00:20:34 -0400

    update

--- image_updates.lua
+++ image_updates.lua
@@ -1,5 +1,4 @@
 localPath = scriptPath()
 imagePath = (localPath .. "image/")
-httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/cancel_assign.png", imagePath .."cancel_assign.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/original.png", imagePath .."original.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/list.txt", imagePath .."list.txt")
