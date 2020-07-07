d7a92cc5b2910c583cebee21a96889283028ed51
iamafkbot <iamafkbot@gmail.com>
2020-07-07 14:41:19 -0400

    update

--- image_updates.lua
+++ image_updates.lua
@@ -8,3 +8,4 @@ httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/in
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/complete_all_zero.png", imagePath .."complete_all_zero.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/mega_growth.png", imagePath .."mega_growth.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/menu_bars.png", imagePath .."menu_bars.png")
+httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/confirm2.png", imagePath .."confirm2.png")
