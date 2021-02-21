5ba37274eb67ab0df7466b1f64bdbd918a99e051
iamafkbot <iamafkbot@gmail.com>
2021-02-21 13:04:18 -0500

    update

--- image_updates.lua
+++ image_updates.lua
@@ -96,4 +96,5 @@ httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/ap
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/apple_id.png", imagePath .."apple_id.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/free_talish.png", imagePath .."free_talish.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/brs_small.png", imagePath .."brs_small.png")
+httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/tent2.png", imagePath .."tent2.png")
 
