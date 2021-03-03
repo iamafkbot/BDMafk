b747cc07db10cf28793fa70280f914136dcc1c27
iamafkbot <iamafkbot@gmail.com>
2021-03-03 11:09:24 -0500

    update

--- image_updates.lua
+++ image_updates.lua
@@ -1,3 +1,4 @@
 localPath = scriptPath()
 imagePath = (localPath .. "image/")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/camp_view.png", imagePath .."camp_view.png")
+httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/ancient_guardian.png", imagePath .."ancient_guardian.png")
