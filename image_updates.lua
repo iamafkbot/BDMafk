1560472207a98813ef2eb183feeb005a903792cc
iamafkbot <iamafkbot@gmail.com>
2021-10-29 12:16:36 -0400

    update

--- image_updates.lua
+++ image_updates.lua
@@ -198,3 +198,4 @@ httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/to
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/dailyreward.png", imagePath .."dailyreward.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/boss_rush_normal.png", imagePath .."boss_rush_normal.png")
 httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/google_play_ok.png", imagePath .."google_play_ok.png")
+httpDownload("https://raw.githubusercontent.com/iamafkbot/BDMafk/master/image/exit.png", imagePath .."exit.png")
